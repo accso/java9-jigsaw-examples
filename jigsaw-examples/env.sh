a9f76ef89c7e097a0ac32368794d794574ede18f
Martin Lehmann <martin.lehmann@accso.de>
2016-08-27 15:24:59 +0200

    Java 9 Jigsaw Examples

--- jigsaw-examples/env.sh
+++ jigsaw-examples/env.sh
@@ -1,11 +1,12 @@
 # Environment settings
 
 # configure paths here
-JAVA_HOME=TODO/path/to/java/home/goes/here
-GRAPHVIZ_HOME=TODO/path/to/graphviz2.38/home/goes/here
-DEPVIS_HOME=TODO/path/to/depvis/goes/here
+# configure paths here
+JAVA_HOME=/a/JDK/jdk1.9.0_ea-b127-x64_20160721_build5308
+GRAPHVIZ_HOME=/a/GraphViz/graphviz-2.38  
+DEPVIS_HOME=j:/github-java9-jigsaw-depvis/depvis
 # \; on Windows (even when in bash), : on Un*x
-PATH_SEPARATOR=TODO
+PATH_SEPARATOR=\;
 
 # ---------------------------------------------------------
 # no need to change anything beyond this line
