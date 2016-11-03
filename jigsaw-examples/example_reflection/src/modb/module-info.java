a6e6473923187432c1cc760decece0b435867275
Kristine Schaal <kristine.schaal@gmx.de>
2016-11-03 08:18:02 +0100

    adopted to open modules

--- jigsaw-examples/example_reflection/src/modb/module-info.java
+++ jigsaw-examples/example_reflection/src/modb/module-info.java
@@ -1,16 +1,18 @@
 module modb {
     // exports not enough anymore for setAccessible(true) when using reflection.
-    // exports pkgb;
+    // opens: since b142 needed for Deep Reflection
+    exports  pkgb to modmain;
+    opens  pkgb to modmain;
+    
+    // exports only allows Shallow Reflection
+    exports pkgb1 to modmain;
 
-    // needed since b136
-    exports private pkgb to modmain;
-
-    // if the following "exports dynamic" is not set, then we get the following runtime exception
+    // if the following "opens" is not set, then we get the following runtime exception
     //    java.lang.reflect.InaccessibleObjectException: 
-    //        Unable to make member of class pkgbinternal.InternalB accessible: 
-    //        module modb does not export pkgbinternal to module modmain
+    //         Unable to make pkgbinternal.InternalB() accessible: 
+    //         module modb does not "opens pkgbinternal" to module modmain
     // exports dynamic pkgbinternal; // not supported anymore since b136 (but still in discussion if it comes back)
     
-    // needed since b136
-    exports private pkgbinternal to modmain;
+    // opens: since b142 needed for Deep Reflection
+    opens  pkgbinternal to modmain;
 }
--- jigsaw-examples/example_reflection/src/modmain/pkgmain/Main.java
+++ jigsaw-examples/example_reflection/src/modmain/pkgmain/Main.java
@@ -5,6 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import pkgb.B;
+import pkgb1.B1;
 
 public class Main {
     public static void main(String[] args) throws NoSuchMethodException, SecurityException,
@@ -17,7 +18,7 @@ public class Main {
 
         // ----------------------------------------------------------------------------------------------------
 
-// B is visible outside modb
+// B is visible outside modb and open to Deep Reflection
         B myb = new B();
 
 // public method: normal call possible
@@ -38,6 +39,26 @@ public class Main {
         System.out.println("Main: " + mymain.toString() + ", B: " + m.invoke(myb, "call via reflection"));
 
 
+        // ----------------------------------------------------------------------------------------------------
+
+// B is visible outside modb, but not open
+        B1 myb1 = new B1();
+     // public method: normal call possible
+        System.out.println("Main: " + mymain.toString() + ", B1: " + myb1.doIt("direct call"));
+
+// public method: call via reflection possible
+        m = myb1.getClass().getMethod("doIt", String.class);
+        System.out.println("Main: " + mymain.toString() + ", B1: " + m.invoke(myb1, "call via reflection"));
+
+     // private method: call via reflection not possible
+        m = myb1.getClass().getDeclaredMethod("doItPrivate", String.class);
+        // compiles but does not run because pkgb1 ist not open to modmain:
+        //       Unable to make private java.lang.String pkgb1.B1.doItPrivate(java.lang.String) accessible: 
+        //       module modb does not "opens pkgb1" to module modmain
+        
+        // m.setAccessible(true);
+        // System.out.println("Main: " + mymain.toString() + ", InternalB: " + m.invoke(myb1, "call via reflection"));
+        
         // ----------------------------------------------------------------------------------------------------
 
 // InternalB is *not* visible outside modb, so we need to create an instance via reflection first
