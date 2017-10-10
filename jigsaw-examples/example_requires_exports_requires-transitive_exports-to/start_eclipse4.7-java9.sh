6cef62835a154a6f29d6a092195af9780e3d8106
Martin Lehmann <martin.lehmann@accso.de>
2017-10-10 15:43:59 +0200

    Migrating Eclipse structures to Eclipse 4.7.1a

--- jigsaw-examples/example_requires_exports_requires-transitive_exports-to/start_eclipse4.7-java9.sh
+++ jigsaw-examples/example_requires_exports_requires-transitive_exports-to/start_eclipse4.7-java9.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
-if [ ! -e ../$0 ]; then
-   echo ERROR: ../$0.sh does not exist. 
-   exit 1
-fi
+#!/bin/sh
 
-../$0 $(pwd)
+#
+# Start script for Eclipse 4.7.1 with Java 9
+#
+# Can be run also on Windows from Babun shell as follows:
+#    ./start_eclipse4.7.1-java9.sh
+#
 
+../$0 .
