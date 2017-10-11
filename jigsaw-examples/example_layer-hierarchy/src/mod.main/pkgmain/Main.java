478447d8dc90df095b6e807c07210d406747813d
Martin Lehmann <martin.lehmann@accso.de>
2017-10-11 10:07:23 +0200

    Added path to json file and module lib

--- jigsaw-examples/example_layer-hierarchy/src/mod.main/pkgmain/Main.java
+++ jigsaw-examples/example_layer-hierarchy/src/mod.main/pkgmain/Main.java
@@ -4,13 +4,22 @@ import pkglayer.LayerHierarchy;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+    	String path;
+    	if (args.length == 0) {
+    		// no path given? then use current working directory
+    		path = new java.io.File(System.getProperty("user.dir")).getAbsolutePath();
+    	}
+    	else {
+    		path = new java.io.File(args[0]).getAbsolutePath();
+    	}
+    	
         // parse layer JSON file
-        LayerBuilder layerBuilder = new LayerBuilder("layers_triple_hierarchy.json");
+        LayerBuilder layerBuilder = new LayerBuilder(path + "/layers_triple_hierarchy.json");
         LayerHierarchy.root.addChildLayer(layerBuilder.parseLayerJsonFile(LayerHierarchy.root));
         // System.out.println(LayerHierarchy.root);
         
         // create Jigsaw layers and add modules to them
-        layerBuilder.createJigsawLayers(LayerHierarchy.root, LayerHierarchy.root.getLayer(), "./mlib");
+        layerBuilder.createJigsawLayers(LayerHierarchy.root, LayerHierarchy.root.getLayer(), path + "/mlib");
         LayerHierarchy.printLayerHierarchy(LayerHierarchy.root);
         
         // call module's classes via reflection
