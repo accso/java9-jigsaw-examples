f305176700b47245be71188360e24bbf17604932
Martin Lehmann <martin.lehmann@accso.de>
2017-10-11 10:16:16 +0200

    Migrating Eclipse structures to Eclipse 4.7.1a

--- jigsaw-examples/example_splitpackage/src/modmainbar/module-info.java
+++ jigsaw-examples/example_splitpackage/src/modmainbar/module-info.java
@@ -4,7 +4,7 @@ module modmainbar {
 
     // we hence do not require modsplitbar2:
     //    requires modsplitbar2;
-    // but we use --add-modules in our run script with which then will prodcue a RuntimeException
+    // but we use --add-modules in our run script / launch configuration -> produces a java.lang.LayerInstantiationException during startup
 
 	// allow reflective access, e.g. in the jerry mouse appserver
 	opens pkgmainbar;
