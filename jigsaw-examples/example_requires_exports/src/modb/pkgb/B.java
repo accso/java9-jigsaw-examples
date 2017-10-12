8317e9dcef56fd9dcf67bdab7fc9183f98b7ddc4
kristines <kristine.schaal@accso.de>
2017-10-12 17:46:57 +0200

    cleanup

--- jigsaw-examples/example_requires_exports/src/modb/pkgb/B.java
+++ jigsaw-examples/example_requires_exports/src/modb/pkgb/B.java
@@ -7,6 +7,7 @@ public class B {
         return "from B";
     }
 
+    // Note the compiler warning: warning: [exports] class C in module modc may not be visible to all clients that require this module
     public C getMyC() {
         return new C();
     }
