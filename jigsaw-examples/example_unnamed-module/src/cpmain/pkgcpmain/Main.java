672b8a22fe837a099084be6ac2b61c5d8a040019
Martin Lehmann <martin.lehmann@accso.de>
2017-10-13 10:51:13 +0200

    Refactoring unnamed module example

--- jigsaw-examples/example_unnamed-module/src/cpmain/pkgcpmain/Main.java
+++ jigsaw-examples/example_unnamed-module/src/cpmain/pkgcpmain/Main.java
@@ -5,18 +5,42 @@ package pkgcpmain;
  */
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("We are calling various classes from " + Main.class + " (which is on the classpath, i.e. in the unnamed module)");
         
-    	// We access a class which is on the classpath, should always work
-    	pkgboncp.BFromClasspath myB1 = new pkgboncp.BFromClasspath();
-        System.out.println("Calling B which is on the classpath: " + myB1.doIt());
-
         // We access a class on the module path, in module modb - class's package is exported, should work
-        pkgb.BFromModule myB2 = new pkgb.BFromModule();
-        System.out.println("Calling an exported class B which is in module modb: " + myB2.doIt());
+        System.out.println("1. Calling an exported class which is in a module on the module path");
+        pkgb.BFromModule myB1 = new pkgb.BFromModule();
+        System.out.println("Calling an exported class B which is in module modb: " + myB1.doIt());
 
         // We access a class on the module path, in module modb - class's package is not exported, results in an java.lang.IllegalAccessError
-        pkgbinternal.BFromModuleButInternal myB3 = new pkgbinternal.BFromModuleButInternal();
-        System.out.println("Calling an internal, non-exported class B which is in module modb: " + myB3.doIt());
+        try {
+            System.out.println("2. Calling an internal, non-exported class which is in a module on the module path");
+
+            pkgbinternal.BFromModuleButInternal myB2 = new pkgbinternal.BFromModuleButInternal();
+            System.out.println("Calling an internal, non-exported class B which is in module modb: " + myB2.doIt());
+        }
+        catch (java.lang.IllegalAccessError err) {
+            err.printStackTrace(System.err);     // we expect a java.lang.IllegalAccessError
+        }
+
+        // ----------------------------------------------------------------------------------------------------------------------------------
+
+    	// We access a class which is on the classpath, should always work
+        System.out.println("3. Calling a class which is on the classpath");
+    	pkgboncp.BFromClasspath myB3 = new pkgboncp.BFromClasspath();
+        System.out.println("Calling B which is on the classpath: " + myB3.doIt());
+
+        try {
+             System.out.println("4. Calling a class which is on the classpath, but whose package name is covered by another class on the module path");
+
+    	     // We access a class which is on the classpath - will not work, but only because the package name "pkgb" is covered by the same class in the same package in modb
+          	 pkgb.BFromClasspath myB4 = new pkgb.BFromClasspath();
+             System.out.println("Calling B which is on the classpath: " + myB4.doIt());
+        }
+        catch (Exception ex) {
+            ex.printStackTrace(System.err);     // we expect a java.lang.ClassNotFoundException
+        }
+
     }
 }
