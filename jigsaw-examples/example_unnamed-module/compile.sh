4f350201a67522fe87c231d8b947cb9df44886f9
kristines <kristine.schaal@accso.de>
2017-10-20 14:34:55 +0200

    Extended example with shaded class

---
+++ jigsaw-examples/example_unnamed-module/compile.sh
@@ -0,0 +1,3 @@
+# Modules need to be compiled before sourcecode for "the classpath"
+sh ./compile-modules.sh
+sh ./compile-cp.sh
--- jigsaw-examples/example_unnamed-module/src/cpmain/pkgcpmain/Main.java
+++ jigsaw-examples/example_unnamed-module/src/cpmain/pkgcpmain/Main.java
@@ -7,11 +7,15 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("We are calling various classes from " + Main.class + " (which is on the classpath, i.e. in the unnamed module)");
-        
+
+        // ----------------------------------------------------------------------------------------------------------------------------------
+
         // We access a class on the module path, in module modb - class's package is exported, should work
         System.out.println("1. Calling an exported class which is in a module on the module path");
         pkgb.BFromModule myB1 = new pkgb.BFromModule();
         System.out.println("Calling an exported class B which is in module modb: " + myB1.doIt());
+
+        // ----------------------------------------------------------------------------------------------------------------------------------
 
         // We access a class on the module path, in module modb - class's package is not exported, results in an java.lang.IllegalAccessError
         try {
@@ -29,18 +33,25 @@ public class Main {
     	// We access a class which is on the classpath, should always work
         System.out.println("3. Calling a class which is on the classpath");
     	pkgboncp.BFromClasspath myB3 = new pkgboncp.BFromClasspath();
-        System.out.println("Calling B which is on the classpath: " + myB3.doIt());
+        System.out.println("Calling BFromClasspath which is on the classpath: " + myB3.doIt());
 
         try {
              System.out.println("4. Calling a class which is on the classpath, but whose package name is covered by another class on the module path");
 
     	     // We access a class which is on the classpath - will not work, but only because the package name "pkgb" is covered by the same class in the same package in modb
           	 pkgb.BFromClasspath myB4 = new pkgb.BFromClasspath();
-             System.out.println("Calling B which is on the classpath: " + myB4.doIt());
+             System.out.println("ERROR: Calling BFromClasspath whose package is both on the module path and on the classpath - SHOULD NOT WORK: " + myB4.doIt());
         }
-        catch (Exception ex) {
+        catch (Throwable ex) {
             ex.printStackTrace(System.err);     // we expect a java.lang.ClassNotFoundException
         }
 
+        // ----------------------------------------------------------------------------------------------------------------------------------
+                
+        System.out.println("5. Calling a class which is on the classpath, but whose class and package name is covered by another class on the module path");
+
+        // We access a class which is on the classpath - will not work, but only because the package name "pkgb" is covered by the same class in the same package in modb
+        pkgb.B myB5 = new pkgb.B();
+        System.out.println("Calling B which is both on the module path and on the classpath: " + myB5.doIt());
     }
 }
