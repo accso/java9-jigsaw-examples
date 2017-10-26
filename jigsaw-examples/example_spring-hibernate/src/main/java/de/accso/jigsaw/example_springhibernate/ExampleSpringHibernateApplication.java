776183cabb225e25db1bf317dc8c0af4be22b070
Martin Lehmann <martin.lehmann@accso.de>
2017-10-26 21:04:57 +0200

    New example for Spring Boot, Web, Security etc - initial checkin as downloaded from start.spring.io - works with JDK 1.8.0-144 (compile, test)

---
+++ jigsaw-examples/example_spring-hibernate/src/main/java/de/accso/jigsaw/example_springhibernate/ExampleSpringHibernateApplication.java
@@ -0,0 +1,12 @@
+package de.accso.jigsaw.example_springhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringHibernateApplication.class, args);
+	}
+}
---
+++ jigsaw-examples/example_spring-hibernate/src/test/java/de/accso/jigsaw/example_springhibernate/ExampleSpringHibernateApplicationTests.java
@@ -0,0 +1,16 @@
+package de.accso.jigsaw.example_springhibernate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleSpringHibernateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
