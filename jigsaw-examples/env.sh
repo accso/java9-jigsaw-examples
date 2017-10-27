1dc89704351faad3fa70dd7527780a14fca99f12
Martin Lehmann <martin.lehmann@accso.de>
2017-10-27 14:47:54 +0200

    Environment variable now all exported, added Gradle and Maven3.5.2 variables

--- jigsaw-examples/env.sh
+++ jigsaw-examples/env.sh
@@ -1,14 +1,19 @@
 # Environment settings
 
 # configure paths here
-JAVA_HOME=TODO/path/to/java9-jdk/goes/here
-GRAPHVIZ_HOME=TODO/path/to/graphviz2.38/goes/here
-MAVEN_HOME=TODO/path/to/Maven3.5.0/goes/here
-DEPVIS_HOME=TODO/path/to/depvis/goes/here
-ECLIPSE_HOME=TODO/path/to/eclipse4.7.1.a/goes/here
+export JAVA_HOME=TODO/path/to/java9-jdk/goes/here
+export GRAPHVIZ_HOME=TODO/path/to/graphviz2.38/goes/here
+export MAVEN_HOME=TODO/path/to/Maven3.5.2/goes/here
+export GRADLE_HOME=TODO/path/to/Gradle4.2.1/goes/here
+export DEPVIS_HOME=TODO/path/to/depvis/goes/here
+export ECLIPSE_HOME=TODO/path/to/eclipse4.7.1.a/goes/here
 
 # \; on Windows (even when in bash), : on Un*x
 PATH_SEPARATOR=TODO
+
+# ---------------------------------------------------------
+
+export M2_HOME=${MAVEN_HOME}
 
 # ---------------------------------------------------------
 
@@ -24,7 +29,7 @@ JAVADOC_OPTIONS=""
 # ---------------------------------------------------------
 # no need to change anything beyond this line
 
-PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH
 
 # helper echo to highlight errors on the terminal
 function myecho {
