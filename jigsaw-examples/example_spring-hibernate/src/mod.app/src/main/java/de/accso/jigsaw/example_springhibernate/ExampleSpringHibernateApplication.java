2d5b8071ae42f737bc7c03d8ece1e27c023831cf
kristines <kristine.schaal@accso.de>
2017-10-30 14:44:10 +0100

    run script to start application from cli (as fat jar); minor debug output in application

--- jigsaw-examples/example_spring-hibernate/src/mod.app/src/main/java/de/accso/jigsaw/example_springhibernate/ExampleSpringHibernateApplication.java
+++ jigsaw-examples/example_spring-hibernate/src/mod.app/src/main/java/de/accso/jigsaw/example_springhibernate/ExampleSpringHibernateApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExampleSpringHibernateApplication {
 
 	public static void main(String[] args) {
+		System.out.println("ExampleSpringHibernateApplication: Starting main class");
 		SpringApplication.run(ExampleSpringHibernateApplication.class, args);
 	}
 }
