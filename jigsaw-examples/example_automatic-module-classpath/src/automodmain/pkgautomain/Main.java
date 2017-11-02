da83970f53f16aeaf1f316b47a2679191fb91d4e
Martin Lehmann <martin.lehmann@accso.de>
2017-11-02 19:25:07 +0100

    Cleanup

--- jigsaw-examples/example_automatic-module-classpath/src/automodmain/pkgautomain/Main.java
+++ jigsaw-examples/example_automatic-module-classpath/src/automodmain/pkgautomain/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		//---- calling class A from classpath.
-		// pkga from Unnamed Module is exported to automatic moduels
+		// pkga from Unnamed Module is exported to Automatic Modules only
 		
 		A myA = new A();
 		System.out.println("\nCalling A.doIt(): " + myA.doIt());
