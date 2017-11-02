adb96dffb8ea5d418c51098b5160309a89c43435
Martin Lehmann <martin.lehmann@accso.de>
2017-11-02 19:24:13 +0100

    Comments to explain compile problems in Eclipse

--- jigsaw-examples/example_addExports_manifest/src/modmain/pkgmain/Main.java
+++ jigsaw-examples/example_addExports_manifest/src/modmain/pkgmain/Main.java
@@ -4,9 +4,11 @@ import jdk.internal.misc.SharedSecrets;
 
 public class Main {
     public static void main(String[] args) {
+    	// Runtime option needed: --add-exports java.base/jdk.internal.misc=modmain
     	SharedSecrets secrets = new SharedSecrets();
         System.out.println("Do you want to know a secret: " + secrets);
 
+    	// Runtime option needed: --add-exports moda/pkgainternal=modmain
         System.out.println(new pkgainternal.A().doIt());
     }
 }
