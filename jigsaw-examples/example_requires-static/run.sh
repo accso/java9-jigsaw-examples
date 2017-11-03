170e6bd32dc7f3030214c771e961e6f22437735e
Martin Lehmann <martin.lehmann@accso.de>
2017-11-03 11:02:42 +0100

    Cleanup, Eclipse dependencies, better comments and explanations, launch cleanup and all run variants as launch files

--- jigsaw-examples/example_requires-static/run.sh
+++ jigsaw-examples/example_requires-static/run.sh
@@ -1,3 +1,7 @@
 . ../env.sh
 
+# --add-modules for both modb and modc is needed as otherwise a java.lang.ClassNotFoundException is thrown because classes from modb/pkgb and modc/pkgc are not found
+# $JAVA_HOME/bin/java $JAVA_OPTIONS --module-path mlib --module modmain/pkgmain.Main | myecho
+
+# because of the 'requires static' dependencies from modb->modc and modmain->modb, we need to add modb and modc explicitely to the runtime Configuration
 $JAVA_HOME/bin/java $JAVA_OPTIONS --module-path mlib --add-modules modb,modc --module modmain/pkgmain.Main | myecho
