6bab72d1816d20cca90851148eea0d1ec187b2ec
Martin Lehmann <martin.lehmann@accso.de>
2017-11-03 10:05:07 +0100

    Cleanup, Eclipse dependencies, better comments and explanations

--- jigsaw-examples/example_gradle-project/moda/src/test/java/pkga1/A1Test.java
+++ jigsaw-examples/example_gradle-project/moda/src/test/java/pkga1/A1Test.java
@@ -4,6 +4,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Currently, this test class does not compile in Eclipse but works fine when called from gradle
+ * on the command line, see script run-test.sh
+ * 
+ * TODO: fix this problem - note that junit has been already added to moda's dependencies on the module path
+ */
 public class A1Test {
     @Test
     public void doIt() {
