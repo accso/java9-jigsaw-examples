57b48e6830e630c7d6e486deee35d10742be09ef
Martin Lehmann <martin.lehmann@accso.de>
2017-11-03 10:53:27 +0100

    Cleanup, Eclipse dependencies, better comments and explanations, launch cleanup and all run variants as launch files

--- jigsaw-examples/example_requires_exports-to/src/modc/module-info.java
+++ jigsaw-examples/example_requires_exports-to/src/modc/module-info.java
@@ -1,6 +1,6 @@
 module modc {
     // Does meanwhile work in Eclipse even though modb1 and modb2 are separate Eclipse projects:
-    //    Note that no dependency from modc to modb1/2 is necessary just to get this "export to" compiled
-	//    so we do not run into problems with cyclic dependencies
+    //    Note that no longer a dependency from modc to modb1/2 is necessary just to get this "export to" compiled
+	//    so we do not run into problems with forbidden cyclic dependencies any more.
     exports pkgc to modb1, modb2;
 }
