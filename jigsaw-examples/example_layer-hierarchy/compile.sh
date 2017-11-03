9f39574fa84d8e8ddbb839e58006771eaad4a7f4
Martin Lehmann <martin.lehmann@accso.de>
2017-11-03 14:46:53 +0100

    Fix, cleanup

--- jigsaw-examples/example_layer-hierarchy/compile.sh
+++ jigsaw-examples/example_layer-hierarchy/compile.sh
@@ -3,9 +3,6 @@
 mkdir -p mods
 mkdir -p mlib 
 
-mkdir -p amlib 
-cp -f src/mod.main/lib/javax.json-1.0.4.jar amlib/
-
 # Compile the mod.x* modules first (separate as they do not compile together with mod.main because of their split package problem,
 #    which is caused by the mod.main using the automatic module amlib/javax.json -> has an automatic reads relationship to all modules
 #    hence causing this stupid compile problem)
