e996c1b81c137aedd20c0590eab1988b4ce6fa5a
Martin Lehmann <martin.lehmann@accso.de>
2017-11-03 08:54:21 +0100

    Cleanup, Eclipse dependencies, better comments and explanations

--- jigsaw-examples/example_addReads_addExports_reflection/src/modmain/pkgmain/Main.java
+++ jigsaw-examples/example_addReads_addExports_reflection/src/modmain/pkgmain/Main.java
@@ -8,6 +8,15 @@ import java.util.Optional;
 
 import pkgb.BExportHelper;
 
+/**
+ * This class cannot be compiled in Eclipse as compiler options --add-exports are needed.
+ * See compile.sh for details.
+ * 
+ * But if compilation has taken place outside Eclipse with the script,
+ * the modular JARs can be found in .../mlib
+ * 
+ * The Eclipse launch files takes the compiled code from there and can be run in Eclipse.
+ */
 public class Main {
     public static void main(String[] args) throws Exception {
         Main mymain = new Main();
@@ -21,19 +30,21 @@ public class Main {
         Module modb = optMod.get();
 
         // -----------------------------------------------
-        // addReads modmain=modb
+        // Implement the 'addReads modmain=modb'
         //    note that this is caller-sensitive, needs to be done in modmain, i.e. here
         System.out.println("Main: add reads of " + modmain.getName() + " to " + modb.getName());
         modmain.addReads(modb);
         
         // -----------------------------------------------
-        // addExports modb/pkgb=modmain
-        //    note that this is caller-sensitive, needs to be done in a class from module modb
+        // Ask for the 'addExports modb/pkgb=modmain'
+        // Note that this is caller-sensitive, so we need this to in a class which belongs to module modb
+        // If we would call the addExports from here directly like with:
+        //      BExportHelper.class.getModule().addExports("pkgbinternal", modmain);
+        // we would produce a java.lang.IllegalCallerException: module modmain != module modb
+        
+        // So instead we ask the BExportHelper to get access to the internal package pkgbinternal.
+        // For that, Java Compiler and also Runtime option needed first: --add-exports modb/pkgb=modmain
         BExportHelper.addExports("pkgbinternal", modmain);
-
-        // The following line is caller-sensitive, must be called from modb and hence would produce a RuntimeException here in modmain
-        //        java.lang.IllegalCallerException: module modmain != module modb
-        // BExportHelper.class.getModule().addExports("pkgbinternal", modmain);
         
         // -----------------------------------------------
         // get an instance of pkgb.B and call its doIt() method
