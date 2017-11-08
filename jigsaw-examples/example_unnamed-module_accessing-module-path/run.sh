8ab08c1c8ad10a0169e8df84c26d25ab0d31ba20
Martin Lehmann <martin.lehmann@accso.de>
2017-11-08 15:45:49 +0100

    Cleanup

--- jigsaw-examples/example_unnamed-module_accessing-module-path/run.sh
+++ jigsaw-examples/example_unnamed-module_accessing-module-path/run.sh
@@ -1,3 +1,7 @@
 . ../env.sh
 
-$JAVA_HOME/bin/java $JAVA_OPTIONS --module-path mlib -cp cplib/cpmain.jar${PATH_SEPARATOR}cplib/cpb.jar --add-modules modb pkgcpmain.Main 2>&1 | myecho
+$JAVA_HOME/bin/java $JAVA_OPTIONS \
+        --module-path mlib \
+        --class-path cplib/cpmain.jar${PATH_SEPARATOR}cplib/cpb.jar \
+        --add-modules modb pkgcpmain.Main \
+        2>&1 | myecho
