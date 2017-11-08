1a67aa3060a5aff64d04e09c4f4858aee0e5f037
Martin Lehmann <martin.lehmann@accso.de>
2017-11-08 16:04:45 +0100

    Access to the classpath can also be given programmatically 'in code'

--- jigsaw-examples/example_unnamed-module_access-from-explicit-module/run.sh
+++ jigsaw-examples/example_unnamed-module_access-from-explicit-module/run.sh
@@ -1,8 +1,9 @@
 . ../env.sh
 
+#      --add-reads modmain=ALL-UNNAMED \
+
 $JAVA_HOME/bin/java $JAVA_OPTIONS \
       --module-path mlib \
       --class-path cplib/cpb.jar \
-      --add-reads modmain=ALL-UNNAMED \
       --module modmain/pkgmain.Main \
       2>&1 | myecho
--- jigsaw-examples/example_unnamed-module_access-from-explicit-module/src/modmain/pkgmain/Main.java
+++ jigsaw-examples/example_unnamed-module_access-from-explicit-module/src/modmain/pkgmain/Main.java
@@ -1,19 +1,26 @@
 package pkgmain;
 
 /**
- * This class cannot be compiled in Eclipse as compiler options --add-reads modmain=ALL-UNNAMED are needed.
+ * This class cannot be compiled in Eclipse as compiler option '--add-reads modmain=ALL-UNNAMED' is needed.
  * See compile.sh for details. 
  */
 public class Main {
     public static void main(String[] args) {
         // ---------------------------------------------------------------
-    	// accessing classes on the classpath    	
-        System.out.println(new pkgboncp.BFromClasspath().doIt());   // Only compiles and runs with command line option --add-reads modmain=ALL-UNNAMED
-        // System.out.println(new pkgb.BFromClasspath().doIt());    // Does not compile because pkgb in cpb is hidden by pkgb of mod b
+    	// accessing from modmain a class on the classpath, in cpb
+        
+    	Main.class.getModule().addReads(Main.class.getClassLoader().getUnnamedModule());	// needed for runtime access to the unnamed module (or alternatively run with launcher option '--add-reads modmain=ALL-UNNAMED')
+    	System.out.println(new pkgboncp.BFromClasspath().doIt());   // Only compiles (outside Eclipse) with command line option --add-reads modmain=ALL-UNNAMED 
+    	
+        // ---------------------------------------------------------------
+        // accessing from modmain a class on the module path, in module modb        
+        System.out.println(new pkgb.B().doIt());					// The class is taken from modb, not from cpb!
 
         // ---------------------------------------------------------------
-        // accessing classes on the module path, to module modb
-        System.out.println(new pkgb.B().doIt());					// The class is taken from modb, not from cpb!
+        // accessing from modmain some classes whose package is both on the classpath and on the module path in modb
+        // Note that only the package contents in modb can be seen here!
+
         System.out.println(new pkgb.BFromModule().doIt());
+        // System.out.println(new pkgb.BFromClasspath().doIt());    // Does not compile because pkgb in cpb is hidden by pkgb of modb (and the latter does not contain pkgb.BFromClasspath)
     }
 }
