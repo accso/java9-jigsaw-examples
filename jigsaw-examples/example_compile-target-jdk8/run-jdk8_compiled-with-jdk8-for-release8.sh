24e6f60c59f8a9a26cd140029bbb5a343a1ab273
Martin Lehmann <martin.lehmann@gmx.de>
2018-07-09 13:24:35 +0200

    Update run-jdk8_compiled-with-jdk8-for-release8.sh

--- jigsaw-examples/example_compile-target-jdk8/run-jdk8_compiled-with-jdk8-for-release8.sh
+++ jigsaw-examples/example_compile-target-jdk8/run-jdk8_compiled-with-jdk8-for-release8.sh
@@ -3,7 +3,7 @@
 echo "Running the application with JDK8, compiled with JDK9 for release 9... This will *not* work but produce a runtime error message similar to this:"
 echo "java.lang.UnsupportedClassVersionError: pkgmain/Main has been compiled by a more recent version of the Java Runtime (class file version 53.0), this version of the Java Runtime only recognizes class file versions up to 52.0"
 
-echo "$JAVA_HOME_JDK8/bin/java $JAVA_OPTIONS  -cp cplib/cpmain-jdk9.jar  pkgmain.Main"
+echo "$JAVA8_HOME/bin/java $JAVA_OPTIONS  -cp cplib/cpmain-jdk9.jar  pkgmain.Main"
 
-$JAVA_HOME_JDK8/bin/java $JAVA_OPTIONS  -cp cplib/cpmain-jdk9.jar  pkgmain.Main 2>&1 | myecho
+$JAVA8_HOME/bin/java $JAVA_OPTIONS  -cp cplib/cpmain-jdk9.jar  pkgmain.Main 2>&1 | myecho
 
